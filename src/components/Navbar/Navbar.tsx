218fb41b31e76300f61036fc679ab660018bcb98
Anderson Malhero de Carvalho <anderson.malheiro@aluno.fapce.edu.br>
2021-04-30 20:43:05 -0300

    Creating navbar component

---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,24 @@
+import React, { useEffect } from "react";
+import { Flex, Link } from "@chakra-ui/react";
+import { ColorModeSwitcher } from "../../ColorModeSwitcher";
+import { BreadcumbBuilder } from "../BreadcumbBuilder/BreadcumbBuilder";
+
+export const Navbar = () => {
+  return (
+    <Flex
+      bg="white"
+      w="100%"
+      h="50px"
+      p={4}
+      as="nav"
+      shadow="md"
+      align="center"
+      justify="space-between"
+      flex="1"
+    >
+      <BreadcumbBuilder basePath="admin" />
+
+      <ColorModeSwitcher justifySelf="flex-end" />
+    </Flex>
+  );
+};
