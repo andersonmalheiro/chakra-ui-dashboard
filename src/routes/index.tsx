09bff605bfa4b0190e05aadcdf1ccc9d8369443d
Anderson Malhero de Carvalho <anderson.malheiro@aluno.fapce.edu.br>
2021-05-02 12:56:58 -0300

    updating router config

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,19 +1,13 @@
 import React from "react";
-import { BrowserRouter, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 import { Admin } from "../pages/Admin";
-
-const Landing = () => (
-  <div>
-    <h1>Landing</h1>
-    <Link to="/admin">Go to Admin</Link>
-  </div>
-);
+import { Login } from "../pages/Login";
 
 export const Routes = () => {
   return (
     <BrowserRouter>
       <Switch>
-        <Route exact path="/" component={Landing} />
+        <Route exact path="/" component={Login} />
         <Route path="/admin" component={Admin} />
       </Switch>
     </BrowserRouter>
