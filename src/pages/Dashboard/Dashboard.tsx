f65b4950a27029ec529da71d5e04c9ae8cd2c2f2
Anderson Malhero de Carvalho <anderson.malheiro@aluno.fapce.edu.br>
2021-05-03 09:50:16 -0300

    Improving dashboard layout

--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -1,12 +1,70 @@
-import React from "react";
-import { Box, Heading } from "@chakra-ui/react";
+import React from 'react';
+import { Box, Button, Grid, Heading, HStack, Text } from '@chakra-ui/react';
+import { IndicatorCard } from '../../components/IndicatorCard';
+import { RiEqualizerLine } from 'react-icons/ri';
 
 export const Dashboard = () => {
   document.title = 'Chakra Admin - Dashboard';
-  
+
   return (
-    <Box fontSize="xl" p={3}>
-      <Heading>Dashboard</Heading>
+    <Box
+      w="full"
+      h="full"
+      overflow="hidden"
+      overflowY="auto"
+      p={4}
+      bg="gray.100"
+    >
+      <HStack justifyContent="space-between" mb="20px">
+        <Heading fontSize="xl">Dashboard</Heading>
+        <Button size="sm">
+          <HStack spacing="5px">
+            <RiEqualizerLine size="18" />
+            <Text>Filters</Text>
+          </HStack>
+        </Button>
+      </HStack>
+
+      <Grid
+        gap="4"
+        w="full"
+        templateRows="auto auto"
+        alignItems="unset"
+        spacing="4"
+      >
+        <Grid gap="4" templateColumns="repeat(auto-fill, minmax(150px, 1fr))">
+          <IndicatorCard />
+          <IndicatorCard />
+          <IndicatorCard />
+        </Grid>
+
+        <Grid gap="4" templateColumns="repeat(auto-fill, minmax(350px, 1fr))">
+          <Box
+            borderRadius="2xl"
+            bg="white"
+            boxShadow="sm"
+            h="full"
+            minH="72"
+            p={4}
+          ></Box>
+          <Box
+            borderRadius="2xl"
+            bg="white"
+            boxShadow="sm"
+            h="full"
+            minH="72"
+            p={4}
+          ></Box>
+          <Box
+            borderRadius="2xl"
+            bg="white"
+            boxShadow="sm"
+            h="full"
+            minH="72"
+            p={4}
+          ></Box>
+        </Grid>
+      </Grid>
     </Box>
   );
 };
