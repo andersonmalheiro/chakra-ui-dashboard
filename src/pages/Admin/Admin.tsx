02fdbe78f2831eae236791272b173a2144de9b8c
Anderson Malhero de Carvalho <anderson.malheiro@aluno.fapce.edu.br>
2021-05-03 09:50:02 -0300

    Removing extra code from dashboard

--- src/pages/Admin/Admin.tsx
+++ src/pages/Admin/Admin.tsx
@@ -1,24 +1,20 @@
-import React from "react";
-import { Box } from "@chakra-ui/layout";
-import { Route, Switch, useRouteMatch } from "react-router-dom";
-import { withLayout } from "../../hocs";
-import { Dashboard } from "../Dashboard";
-import { Home } from "../Home";
+import React from 'react';
+import { Box } from '@chakra-ui/layout';
+import { Route, Switch, useRouteMatch } from 'react-router-dom';
+import { withLayout } from '../../hocs';
+import { Dashboard } from '../Dashboard';
+import { Home } from '../Home';
 
 export const Admin = withLayout(() => {
   document.title = 'Chakra Admin - Dashboard';
 
   const { path, url } = useRouteMatch();
   return (
-    <Box h="full" w="full" p={4}>
-      <h1>Admin</h1>
-
-      <Box>
-        <Switch>
-          <Route exact path={path} component={Home} />
-          <Route path={`${url}/dashboard`} component={Dashboard} />
-        </Switch>
-      </Box>
+    <Box h="full" w="full">
+      <Switch>
+        <Route exact path={path} component={Home} />
+        <Route path={`${url}/dashboard`} component={Dashboard} />
+      </Switch>
     </Box>
   );
 });
