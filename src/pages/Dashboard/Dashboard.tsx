fbd0d98807f48bf763e55c28f3a2fb0f3bb8a584
Anderson Malhero de Carvalho <anderson.malheiro@aluno.fapce.edu.br>
2021-06-21 15:24:19 -0300

    Adding chart lib and examples

--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -10,10 +10,140 @@ import {
 } from '@chakra-ui/react';
 import { IndicatorCard } from '../../components/IndicatorCard';
 import { RiEqualizerLine } from 'react-icons/ri';
+import { useRef } from 'react';
+import Chart from 'chart.js/auto';
+import { useEffect } from 'react';
 
 export const Dashboard = () => {
   document.title = 'Chakra Admin - Dashboard';
+
   const { colorMode } = useColorMode();
+
+  const barChartRef = useRef<HTMLCanvasElement>(null);
+  const pieChartRef = useRef<HTMLCanvasElement>(null);
+  const lineChartRef = useRef<HTMLCanvasElement>(null);
+
+  const buildCharts = () => {
+    new Chart(barChartRef.current, {
+      type: 'bar',
+      data: {
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [
+          {
+            label: '# of Votes',
+            data: [12, 19, 3, 5, 2, 3],
+            backgroundColor: [
+              'rgba(255, 99, 132, 0.2)',
+              'rgba(54, 162, 235, 0.2)',
+              'rgba(255, 206, 86, 0.2)',
+              'rgba(75, 192, 192, 0.2)',
+              'rgba(153, 102, 255, 0.2)',
+              'rgba(255, 159, 64, 0.2)',
+            ],
+            borderColor: [
+              'rgba(255, 99, 132, 1)',
+              'rgba(54, 162, 235, 1)',
+              'rgba(255, 206, 86, 1)',
+              'rgba(75, 192, 192, 1)',
+              'rgba(153, 102, 255, 1)',
+              'rgba(255, 159, 64, 1)',
+            ],
+            borderWidth: 1,
+          },
+        ],
+      },
+      options: {
+        scales: {
+          y: {
+            beginAtZero: true,
+          },
+        },
+      },
+    });
+
+    new Chart(pieChartRef.current, {
+      type: 'pie',
+      data: {
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [
+          {
+            label: '# of Votes',
+            data: [12, 19, 3, 5, 2, 3],
+            backgroundColor: [
+              'rgba(255, 99, 132, 0.2)',
+              'rgba(54, 162, 235, 0.2)',
+              'rgba(255, 206, 86, 0.2)',
+              'rgba(75, 192, 192, 0.2)',
+              'rgba(153, 102, 255, 0.2)',
+              'rgba(255, 159, 64, 0.2)',
+            ],
+            borderColor: [
+              'rgba(255, 99, 132, 1)',
+              'rgba(54, 162, 235, 1)',
+              'rgba(255, 206, 86, 1)',
+              'rgba(75, 192, 192, 1)',
+              'rgba(153, 102, 255, 1)',
+              'rgba(255, 159, 64, 1)',
+            ],
+            borderWidth: 1,
+          },
+        ],
+      },
+      options: {
+        layout: {
+
+        },
+        scales: {
+          y: {
+            beginAtZero: true,
+          },
+        },
+      },
+    });
+
+    new Chart(lineChartRef.current, {
+      type: 'line',
+      data: {
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [
+          {
+            label: '# of Votes',
+            data: [12, 19, 3, 5, 2, 3],
+            backgroundColor: [
+              'rgba(255, 99, 132, 0.2)',
+              'rgba(54, 162, 235, 0.2)',
+              'rgba(255, 206, 86, 0.2)',
+              'rgba(75, 192, 192, 0.2)',
+              'rgba(153, 102, 255, 0.2)',
+              'rgba(255, 159, 64, 0.2)',
+            ],
+            borderColor: [
+              'rgba(255, 99, 132, 1)',
+              'rgba(54, 162, 235, 1)',
+              'rgba(255, 206, 86, 1)',
+              'rgba(75, 192, 192, 1)',
+              'rgba(153, 102, 255, 1)',
+              'rgba(255, 159, 64, 1)',
+            ],
+            borderWidth: 1,
+          },
+        ],
+      },
+      options: {
+        scales: {
+          y: {
+            beginAtZero: true,
+          },
+        },
+      },
+    });
+  };
+
+  useEffect(() => {
+    if (barChartRef.current) {
+      buildCharts();
+    }
+  }, []);
 
   return (
     <Box
@@ -60,7 +190,9 @@ export const Dashboard = () => {
             h="full"
             minH="72"
             p={4}
-          ></Box>
+          >
+            <canvas ref={barChartRef} width="400" height="400" />
+          </Box>
           <Box
             borderRadius="2xl"
             bg={colorMode === 'dark' ? 'gray.700' : 'white'}
@@ -68,7 +200,9 @@ export const Dashboard = () => {
             h="full"
             minH="72"
             p={4}
-          ></Box>
+          >
+            <canvas ref={pieChartRef} width="400" height="400" />
+          </Box>
           <Box
             borderRadius="2xl"
             bg={colorMode === 'dark' ? 'gray.700' : 'white'}
@@ -76,7 +210,9 @@ export const Dashboard = () => {
             h="full"
             minH="72"
             p={4}
-          ></Box>
+          >
+            <canvas ref={lineChartRef} width="400" height="400" />
+          </Box>
         </Grid>
       </Grid>
     </Box>
